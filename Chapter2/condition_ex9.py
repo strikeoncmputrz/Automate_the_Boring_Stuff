29b7feb22790a9165fd4833f38063320d66b7cff
strikeoncmputrz <strikeoncmputrz@gmail.com>
2015-11-23 17:55:34 -0500

    Completed Chapter 2

---
+++ Chapter2/condition_ex9.py
@@ -0,0 +1,9 @@
+print('Hi there. \n Please provide a number for spam. \n Spam = ...')
+spam = input()
+if spam == 2:
+	print('Howdy')
+elif spam == 1:
+	print('Hello')
+else:
+	print('Greetings\n')
+
---
+++ Chapter2/printNumbers.py
@@ -0,0 +1,7 @@
+for i in range(1, 11):
+	print(str(i))
+i = 1
+while i < 11:
+	print(str(i))
+	i = i + 1
+
