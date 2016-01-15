619907425359f241d96bafdd19edc54f7c0adbac
strikeoncmputrz <strikeoncmputrz@gmail.com>
2016-01-14 20:23:11 -0500

    Completed Madlibs by using regex

--- Chapter8/madLibs.py
+++ Chapter8/madLibs.py
@@ -1,10 +1,13 @@
 #! python3
 #  madlibs.py reads in a text file and asks the user to replace all occurences
-#  of VERB, ADVERB, ADJECTIVE, and NOUN. The program then prints the new madlib to the screen
-#  saves the new madlib to a file if the user also provides a name for the new file. 
+#  of VERB, ADVERB, ADJECTIVE, and NOUN. The program then prints the new madlib to the screen,
+#  and saves the new madlib to a file if the user also provides a name for the new file. 
 
-import os, sys
-	
+import os, sys, re
+
+#Global Regex to match any keyword, can handle punctuation
+addWord = re.compile(r'(VERB|ADVERB|ADJECTIVE|NOUN)([,.!?]){0,1}')
+
 
 #addLib function takes file as arg, calls replacePromt for each line and returns list of
 #updated lines
@@ -14,19 +17,29 @@ def addLib(tfile):
 	for line in tfile:
 		nLine = ''
 		for word in line.split():
-			if word == 'VERB':
-				print("Enter a verb:")
-				word = input()
-			elif word == 'ADVERB':
-				print("Enter an adverb:")
-				word = input()
-			elif word == 'ADJECTIVE':
-				print("Enter an adjective:")
-				word = input()
-			elif word == 'NOUN':
-				print("Enter a noun:")
-				word = input()
-			nLine += ' '+ word
+			wMatch = addWord.search(word)
+			if wMatch:
+				if wMatch.group(1) == 'VERB':
+					print("Enter a verb:")
+					word = input()
+				elif wMatch.group(1) == 'ADVERB':
+					print("Enter an adverb:")
+					word = input()
+				elif wMatch.group(1) == 'ADJECTIVE':
+					print("Enter an adjective:")
+					word = input()
+				elif wMatch.group(1) == 'NOUN':
+					print("Enter a noun:")
+					word = input()
+				
+				if	wMatch.group(2): 
+					nLine += ' '+word+(wMatch.group(2))
+
+				else:
+					nLine += ' '+word
+			
+			else: 
+				nLine += ' '+ word
 		nLine += '\n'
 		outlist.append(nLine)
 
@@ -37,4 +50,8 @@ newList = addLib(tFile)
 tFile.close()
 for line in newList:
 	print(line, end='')
-
+if len(sys.argv) == 3:
+	outfile = open('./'+sys.argv[2], 'w')
+	for line in newList:
+		outfile.write(line)
+	outfile.close()
